ff26c3cfa729f61de2e804e7d4c9d5bf309d73dc
sam@freighttrust.com <sam@freighttrust.com>
2020-07-06 22:59:41 -0700

    feat(v1.0-rc1): eDIFACTand X12 4010/5010/6010 JSON Objects
    Cannonical JSON object representations of EDIFACT and X12

---
+++ tests/generateacknowledgement/enummap.js
@@ -0,0 +1,67 @@
+//Other validation rules files are located in the RDPCrystalInstallation\Validation Rules directory
+function EnumMap() {
+        this.SnipLevelTypes = {
+            0: "DoesNotApply",
+            1: "Integrity",
+            2: "Requirement",
+            3: "Balance",
+            4: "Situational",
+            5: "CodeSet"
+        }
+
+        this.MessageTypes = {
+            0: "None",
+            1: "CompositeElementMissing",
+            2: "CompositeElementDataFormatInvalid",
+            3: "CompositeElementNotUsed",
+            4: "CompositeElementRecommended",
+            5: "CompositeElementNotRecommended",
+            6: "CompositeElementLessThanMinimumLength",
+            7: "CompositeElementGreaterThanMaximumLength",
+            8: "CompositeElementHasWrongValue",
+            9: "CompositeElementValueAlreadyEntered",
+            10: "DuplicateSegment",
+            11: "DuplicateLoop",
+            12: "ElementHasWrongValue",
+            13: "InternalElementHasWrongValue",
+            14: "ElementLessThanMinimumLength",
+            15: "ElementGreaterThanMaximumLength",
+            16: "ElementValueAlreadyEntered",
+            17: "ElementDataFormatInvalid",
+            18: "ElementNotUsed",
+            19: "ElementRecommended",
+            20: "ElementNotRecommended",
+            21: "ElementMissing",
+            22: "EmptyTrailingElements",
+            23: "EmptyTrailingCompositeElement",
+            24: "ExtraElementFound",
+            25: "ExtraCompositeElementFound",
+            26: "ExtraDataAtEndOfFile",
+            27: "LoopMissing",
+            28: "LoopNotUsed",
+            29: "LoopNotRecommended",
+            30: "LoopRecommended",
+            31: "MalformedSegment",
+            32: "InvalidIntegerValue",
+            33: "InvalidDateValue",
+            34: "InvalidTimeValue",
+            35: "InvalidAlphabeticValue",
+            36: "InvalidPositiveNumberValue",
+            37: "InvalidNegativeNumberValue",
+            38: "InvalidDecimalValue",
+            39: "SegmentNotRecommended",
+            40: "SegmentNotUsed",
+            41: "SegmentNotInDictionary",
+            42: "SegmentRecommended",
+            43: "SegmentMissing",
+            44: "SummaryValueMismatch",
+            45: "UnrecognizedLoop",
+            46: "UnrecognizedSegment",
+            47: "IllegalCharactorInFile",
+            48: "MalformedEDIFile",
+            49: "MaximumErrorLimitReached",
+            50: "UnexpectedError"
+        }
+}
+
+module.exports = EnumMap;
---
+++ tests/validatedocument/index.js
@@ -0,0 +1,60 @@
+/// <reference path="../../lib/RDPCrystalEDILibrary.d.ts" />
+const edi = require('../../lib/RDPCrystalEDILibrary').RDPCrystalEDILibrary;
+const fs = require('fs');
+const enumMap = require('./enummap');
+
+let map = new enumMap();
+
+//Create a new validator
+let validator = new edi.EDIValidator();
+
+//While validating also load the data in memory
+validator.LoadValidatedData = true;
+
+//Load a 5010 270 validation rules
+let validationRules = fs.readFileSync('../../validationrules/5010/Rules_5010_270_005010X279A1.Rules').toString();
+
+validator.EDIRulesFileData = validationRules;
+
+//Set the EDI data to validate and load.  This can also be read in from a file
+validator.EDIDataString = "ISA*00*..........*01*SECRET....*ZZ*SUBMITTERS.ID..*ZZ*RECEIVERS.ID...*030101*1253*^*00501*000000905*1*T*:~" +
+    "GS*Hk*SENDER CODE*RECEIVERCODE*19991231*0802*1*X*005010X279A1~" +
+    "ST*271*1234*005010X279A1~" +
+    "BHT*0022*13*10001234*20060501*1319~" +
+    "HL*1**20*1~" +
+    "NM1*PR*2*ABC COMPANY*****PI*842610001~" +
+    "HL*2*1*21*1~" +
+    "NM1*1P*2*BONE AND JOINT CLINIC*****SV*2000035~" +
+    "HL*3*2*22*0~" +
+    "TRN*1*93175-012547*9877281234~" +
+    "NM1*IL*1*SMITH*ROBERT****MI*11122333301~" +
+    "DMG*D8*19430519~" +
+    "DTP*291*D8*20060501~" +
+    "EQ*30~" +
+    "SE*13*1234~" +
+    "GE*1*1~" +
+    "IEA*1*000000905~";
+
+console.log("Validating 5010 271 EDI Files");
+validator.validate();
+
+console.log("Errors Found");
+//Get all errors from the EDI data
+for (let i = 0; i < validator.Errors.Count; i++) {
+    let error = validator.Errors.getItem(i);
+
+    console.log(
+        {
+            Type: "Error",
+            Line: error.LineNumber,
+            Transaction: "",
+            SnipLevel: map.SnipLevelTypes[error.SnipLevel],  //SnipTestLevel Enum
+            Message: map.MessageTypes[error.Message], //EDIValidationMessage Enum
+            Loop: error.Loop,
+            Segment: error.Segment,
+            Element: error.ElementOrdinal,
+            Composite: error.CompositeElementOrdinal,
+            Description: error.Description,
+            Ordinal: error.SegmentOrdinal
+        });
+}
